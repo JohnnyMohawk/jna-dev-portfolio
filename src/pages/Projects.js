7c635ce3f58aa5b49109c1346e4f1efa1b222eb3
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-08-26 22:32:11 -0500

    add project photos

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,27 @@
+import {useState} from "react"
+import { projects } from "../data/projects"
+
 const Projects = () => {
+    const [cards, setCards] = useState(projects)
     return (
         <>
             <section className="contacts projects">
                 <h1>My Projects</h1>
+                <div className="cards">
+                    {cards.map(({id, title, desc, github, demo, image}) => (
+                        <article key={id}>
+                            <img src={image} alt={title} />
+                            <h4>{title}</h4>
+                            <p>{desc}</p>
+                            <ul>
+                                <li><a href={github} target="_blank" rel="noopenner noreferrer">GitHub</a></li>
+                                <li><a href={demo} target="_blank" rel="noopenner noreferrer">Deployed Link</a></li>
+                            </ul>
+                        </article>
+                    ))}
+                </div>
+
+
             </section>
         </>
     )
