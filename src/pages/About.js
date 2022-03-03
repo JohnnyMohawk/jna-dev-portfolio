14b94efdee1028e74c43835d84ab696a90ac4e9a
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-03-03 10:29:38 -0600

    replace resume pdf with material ui modals

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,9 @@
 import { Link } from 'react-router-dom';
+import ArtDesignModal from '../components/ArtDesignModal';
+import EducationModal from '../components/EducationModal';
+import ExperienceModal from '../components/ExperienceModal';
+import OtherModal from '../components/OtherModal';
+import SkillsModal from '../components/SkillsModal';
 
 const About = () => {
     return (
@@ -10,6 +15,15 @@ const About = () => {
                     <img className="home-logo" id="about-img" src="./images/project-6.png" />
                 </Link>
                 <h3 id="about-header">writer && readerOfCode || maker && doerOfThings<br></br> cooker && eaterOfFood || rescuer && loverOfDogs</h3>
+
+                <div className='modalDiv'>
+                    <SkillsModal />
+                    <ExperienceModal />
+                    <EducationModal />
+                    <ArtDesignModal />
+                    <OtherModal />
+                </div>
+
                 <p className="bio">I am a full-stack software developer with a passion for problem solving, organization, efficiency, 
                 and creativity. Having spent my early years working as a professional actor on screen and stage, and studying visual art, my creative energy turned 
                 towards the culinary arts and I have logged two decades as a professional chef, working in some of the nation’s finest 
@@ -17,6 +31,9 @@ const About = () => {
                 {/* <div className="pdf">
                     <embed src="John_NelsonAlden_Dev_Resume_for_netlify.pdf" width="100%" height="1120px" />
                 </div> */}
+                
+                
+
             </section>
             </div>
         </>
