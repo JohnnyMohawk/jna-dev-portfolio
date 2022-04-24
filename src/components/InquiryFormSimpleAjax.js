5a336e1674f8601ead9b8d479edf8a28f06eb7a0
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-04-24 00:16:06 -0500

    add form to contact page

---
+++ src/components/InquiryFormSimpleAjax.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import { stringify } from 'qs'
+import { serialize } from 'dom-form-serializer'
+
+import './InquiryForm.css'
+
+const fetch = window.fetch
+
+class Form extends React.Component {
+  static defaultProps = {
+    name: 'Simple Form Ajax',
+    subject: '!!!JohnNelsonAlden.com Form Alert!!!', // optional subject of the notification email
+    action: '',
+    successMessage: 'Thanks for your inquiry, I will get back to you as soon as possible.',
+    errorMessage:
+      'There is a problem, your message has not been sent, please try contacting us via email'
+  }
+
+  state = {
+    alert: '',
+    disabled: false
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.disabled) return
+
+    const form = e.target
+    const data = serialize(form)
+    this.setState({ disabled: true })
+    fetch(form.action + '?' + stringify(data), {
+      method: 'POST'
+    })
+      .then(res => {
+        if (res.ok) {
+          return res
+        } else {
+          throw new Error('Network error')
+        }
+      })
+      .then(() => {
+        form.reset()
+        this.setState({
+          alert: this.props.successMessage,
+          disabled: false
+        })
+      })
+      .catch(err => {
+        console.error(err)
+        this.setState({
+          disabled: false,
+          alert: this.props.errorMessage
+        })
+      })
+  }
+
+  render () {
+    const { name, subject, action } = this.props
+
+    return (
+      <form
+        className='EnquiryForm'
+        name={name}
+        action={action}
+        onSubmit={this.handleSubmit}
+        data-netlify=''
+        data-netlify-honeypot='_gotcha'
+      >
+        {this.state.alert && (
+          <div className='EnquiryForm--Alert'>{this.state.alert}</div>
+        )}
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Name'
+            name='name'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='email'
+            placeholder='Email'
+            name='email'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label has-arrow'>
+          <select
+            className='EnquiryForm--Input EnquiryForm--Select'
+            name='type'
+            defaultValue='Type of Inquiry'
+            required
+          >
+            <option disabled hidden>
+              Type of Inquiry
+            </option>
+            <option>Need to know more</option>
+            <option>Found a bug</option>
+            <option>Want to say hello</option>
+          </select>
+        </label>
+        <label className='EnquiryForm--Label'>
+          <textarea
+            className='EnquiryForm--Input EnquiryForm--Textarea'
+            placeholder='Message'
+            name='message'
+            rows='8'
+            required
+          />
+        </label>
+        <input type='text' name='_gotcha' style={{ display: 'none' }} />
+        {!!subject && <input type='hidden' name='subject' value={subject} />}
+        <input type='hidden' name='form-name' value={name} />
+        <input
+          className='Button EnquiryForm--SubmitButton'
+          type='submit'
+          value='Inquire'
+          disabled={this.state.disabled}
+        />
+      </form>
+    )
+  }
+}
+
+export default Form
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,6 +1,7 @@
 import { FaTwitter, FaEnvelope, FaPalette, FaLinkedin, FaRedditAlien, FaPhoneAlt } from "react-icons/fa"
 import GooeyBG from "../components/Gooey"
 import './contacts.css'
+import InquiryFormSimpleAjax from '../components/InquiryFormSimpleAjax'
 
 const Contacts = () => {
     return (
@@ -8,14 +9,18 @@ const Contacts = () => {
             <section className="contacts" id="contact-page">
             <GooeyBG className="gooeyBg" />
                 <h1 className="pageTitle">Contact Me</h1>
-                <ul>
-                    {/* <li><a href="tel:13129090134"><FaPhoneAlt /> 1  (312) 909 - 0134</a></li> */}
-                    <li><a href="mailto:johnnelsonalden@gmail.com" target="_blank" rel="noopennernoreferrer">< FaEnvelope/> JohnNelsonAlden@gmail.com</a></li>
-                    <li><a href="https://twitter.com/_JohnnyMohawk" target="_blank" rel="noopennernoreferrer"><FaTwitter /> Follow me @_JohnnyMohawk</a></li>
-                    <li><a href="https://hashaxis.com/creator/JohnnyMohawk#onsale" target="_blank" rel="noopennernoreferrer"><FaPalette /> My NFT Art Gallery</a></li>
-                    <li><a href="https://www.reddit.com/user/Johnny_Mohawk" target="_blank" rel="noopennernoreferrer"><FaRedditAlien /> Let's Discuss on Reddit</a></li>
-                    <li><a href="https://www.linkedin.com/in/john-nelson-alden/" target="_blank" rel="noopennernoreferrer"><FaLinkedin /> Talk Shop on Linkedin</a></li>
-                </ul>
+                
+                    <ul>
+                        {/* <li><a href="tel:13129090134"><FaPhoneAlt /> 1  (312) 909 - 0134</a></li> */}
+                        <li><a href="mailto:johnnelsonalden@gmail.com" target="_blank" rel="noopennernoreferrer">< FaEnvelope/> JohnNelsonAlden@gmail.com</a></li>
+                        <li><a href="https://twitter.com/_JohnnyMohawk" target="_blank" rel="noopennernoreferrer"><FaTwitter /> Follow me @_JohnnyMohawk</a></li>
+                        <li><a href="https://hashaxis.com/creator/JohnnyMohawk#onsale" target="_blank" rel="noopennernoreferrer"><FaPalette /> My NFT Art Gallery</a></li>
+                        <li><a href="https://www.reddit.com/user/Johnny_Mohawk" target="_blank" rel="noopennernoreferrer"><FaRedditAlien /> Let's Discuss on Reddit</a></li>
+                        <li><a href="https://www.linkedin.com/in/john-nelson-alden/" target="_blank" rel="noopennernoreferrer"><FaLinkedin /> Talk Shop on Linkedin</a></li>
+                    </ul>
+                <div className="contactWrap">
+                    <InquiryFormSimpleAjax name="Simple Form Ajax" />
+                </div>
             </section>
         </>
     )
